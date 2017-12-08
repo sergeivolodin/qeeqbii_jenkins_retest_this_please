839f0abeee1dea362b46cdce2e3e5369676ca6d9
Sergei Volodin <etoestja@yandex.ru>
2017-12-08 15:39:50 +0100

    Add automerge feature

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-Jenkins auto retest for Qeeqbii team
+### Jenkins auto retest for Qeeqbii team
 
 Do you have a Pull Request open and cannot merge because other people's tests randomly crash? This is the solution
+
+*This script can also be used with other repos, but the very first lines of it must be modified*
 
 1. Create GitHub access token at https://github.com/settings/tokens (need all permissions)
 2. Get your commit id and pull request number (example: 19b196c and 311)
@@ -9,3 +11,4 @@ Do you have a Pull Request open and cannot merge because other people's tests ra
  $ retest_this_please.sh your_username your_access_token your_latest_commit_id your_pr_id
 ```
 4. Wait and see the magic happen! The script will trigger rebuild on each failed build associated with this commit.
+5. On success it will try to merge the PR
--- retest_this_please.sh
+++ retest_this_please.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 usg="Usage: $0 username auth_token commit_id pr_id"
 message="jenkins retest this please"
+owner="sweng-epfl"
+repo="team-qeeq"
 
 if [ "X$4" == "X" ]
 then
@@ -18,14 +20,22 @@ pr_id=$4
 
 while true
 do
-    curl -u $username:$auth_token https://api.github.com/repos/sweng-epfl/team-qeeq/commits/$commit_id/statuses > status.json
+    curl -s -u $username:$auth_token https://api.github.com/repos/$owner/$repo/commits/$commit_id/statuses > status.json
     state=$(python -c "import json; print(json.loads(open('status.json', 'r').read())[0]['state'])")
-    echo "LAST STATE: $state"
+    echo "$(date) LAST STATE: $state"
     if [ "X$state" == "Xfailure" ]
     then
-        echo "Last build failed. Restarting build..."
-        curl -u $username:$auth_token --data "{\"body\": \"$message\"}" https://api.github.com/repos/sweng-epfl/team-qeeq/issues/$pr_id/comments
-        echo "Added comment: [$message]"
+        echo "$(date) Last build failed. Restarting build..."
+        curl -s -u $username:$auth_token --data "{\"body\": \"$message\"}" https://api.github.com/repos/$owner/$repo/issues/$pr_id/comments
+        echo "$(date) Added comment: [$message]"
+    fi
+
+    if [ "X$state" == "Xsuccess" ]
+    then
+        echo "$(date) Congratulations! Your build was successful. Now I will try to merge the PR"
+        curl -s -X PUT -u $username:$auth_token https://api.github.com/repos/$owner/$repo/pulls/$pr_id/merge
+        echo "$(date) Exiting"
+        exit 0
     fi
 
     sleep 10
